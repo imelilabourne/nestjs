a818062ef22d678e05e96fa59f0d339f11116568
elilabourne <elilabourne.detorres@gmail.com>
2023-07-31 17:41:50 +0800

    get request test

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  // getHello(): string {
+  //   return 'Hello World!!!';
+  // }
+
+  getTasks(): any[] {
+    return [
+      { item: "Draw a house", isDone: false}
+    ]
   }
 }
