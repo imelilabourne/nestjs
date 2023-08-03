5eb70c6a8df46eeeb9f9685baea6d3f37abc5fe5
elilabourne <elilabourne.detorres@gmail.com>
2023-08-03 14:49:16 +0800

    return products sample

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,79 @@ export class AppService {
 
   getTasks(): any[] {
     return [
-      { item: "Draw a house", isDone: false}
+      {
+        "code": 3001404307,
+        "desc": "50pcs High Quality U Cups 12oz/16oz/22oz 95mm Plastic Cups Milktea Cups Coffee Cups",
+        "variationId": 73642928695,
+        "variant": "12oz (360ml),Cups Only",
+        "unitPrice": "160",
+        "category": "PLASTIC CUP"
+      },
+      {
+        "code": 3001404307,
+        "desc": "50pcs High Quality U Cups 12oz/16oz/22oz 95mm Plastic Cups Milktea Cups Coffee Cups",
+        "variationId": 73642928696,
+        "variant": "16oz (500ml),Cups Only",
+        "unitPrice": "180",
+        "category": "PLASTIC CUP"
+      },
+      {
+        "code": 3001404307,
+        "desc": "50pcs High Quality U Cups 12oz/16oz/22oz 95mm Plastic Cups Milktea Cups Coffee Cups",
+        "variationId": 73642928697,
+        "variant": "16oz (500ml),Cup + Dome Lids",
+        "unitPrice": "255",
+        "category": "PLASTIC CUP"
+      },
+      {
+        "code": 3001404307,
+        "desc": "50pcs High Quality U Cups 12oz/16oz/22oz 95mm Plastic Cups Milktea Cups Coffee Cups",
+        "variationId": 73642928698,
+        "variant": "22oz (700ml),Cups Only",
+        "unitPrice": "205",
+        "category": "PLASTIC CUP"
+      },
+      {
+        "code": 3001404307,
+        "desc": "50pcs High Quality U Cups 12oz/16oz/22oz 95mm Plastic Cups Milktea Cups Coffee Cups",
+        "variationId": 73642928699,
+        "variant": "22oz (700ml),Cup + Flat Lids",
+        "unitPrice": "270",
+        "category": "PLASTIC CUP"
+      },
+      {
+        "code": 3001404307,
+        "desc": "50pcs High Quality U Cups 12oz/16oz/22oz 95mm Plastic Cups Milktea Cups Coffee Cups",
+        "variationId": 73642928700,
+        "variant": "12oz (360ml),Cup + Flat Lids",
+        "unitPrice": "225",
+        "category": "PLASTIC CUP"
+      },
+      {
+        "code": 3001404307,
+        "desc": "50pcs High Quality U Cups 12oz/16oz/22oz 95mm Plastic Cups Milktea Cups Coffee Cups",
+        "variationId": 192481786592,
+        "variant": "12oz (360ml),Cup + Strawless Lid",
+        "unitPrice": "250",
+        "category": "PLASTIC CUP"
+      },
+      {
+        "code": 3001404307,
+        "desc": "50pcs High Quality U Cups 12oz/16oz/22oz 95mm Plastic Cups Milktea Cups Coffee Cups",
+        "variationId": 192481786593,
+        "variant": "16oz (500ml),Cup + Strawless Lid",
+        "unitPrice": "270",
+        "category": "PLASTIC CUP"
+      },
+      {
+        "code": 3001404307,
+        "desc": "50pcs High Quality U Cups 12oz/16oz/22oz 95mm Plastic Cups Milktea Cups Coffee Cups",
+        "variationId": 192481786594,
+        "variant": "22oz (700ml),Cup + Strawless Lid",
+        "unitPrice": "295",
+        "category": "PLASTIC CUP"
+      }
     ]
+    
   }
 }
