40e82e8127e907e19f082ec903cc3c23df8bf728
elilabourne <elilabourne.detorres@gmail.com>
2023-08-25 14:37:20 +0800

    typeorm connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'password',
+    database: 'nestjs_mysql',
+    entities: [],
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
