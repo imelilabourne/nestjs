efdb6867dd59cae1b5af84e1a67a2eb8ac42ffe4
elilabourne <elilabourne.detorres@gmail.com>
2023-11-10 14:47:19 +0800

    removed func in main controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  // @Get("products")
-  // getTasks() {
-  //   return this.appService.getProducts()
-  // }
-
-  // @Post("product")
-  // addProduct(@Body() addProduct: ProductDto) {
-  //   return this.appService.addProduct(addProduct)
-  // }
-
-  // @Get("product/:variationId")
-  // getOneProduct(@Param('variationId') id: string) {
-  //   return this.appService.getProducts().find(prod => prod.variationId == id)
-  // }
-
-  // @Delete("product/:variationId")
-  // deleteOneProduct(@Param('variationId') id: string) {
-  //   return this.appService.deleteProduct(id)
-  // }
+  constructor() {}
 }
