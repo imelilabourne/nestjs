634537d824366547f492243b6d10076c910ed0a2
elilabourne <elilabourne.detorres@gmail.com>
2023-11-13 21:23:43 +0800

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,11 @@ import { Product } from './typeorm/entities/Product';
 @Module({
   imports: [ TypeOrmModule.forRoot({
     type: 'mysql',
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASS,
-    database: 'nestjs_mysql',
+    host: process.env.MYSQLHOST,
+    port: +process.env.MYSQLPORT,
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     entities: [ Product ],
     synchronize: true
   }), ProductsModule],
